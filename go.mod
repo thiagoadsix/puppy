de03afb13c5e9b79e2625e53cd3b6e905f2ef404
Thiago Andrade Silva <thiago.adsix@gmail.com>
2023-08-10 00:38:20 -0300

    feat: create new functions and import dog module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thiagoadsix/puppy
 
 go 1.20
+
+require github.com/thiagoadsix/dog v0.0.0-20230810033434-d7da9920ad02
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/thiagoadsix/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrouUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrouUp(Barks())
 }
